b1c8ed5c2112e3f8a7da46d06c02098abf9dd654
mariogustavoorrillo <gustavo.orrillo@gmail.com>
2024-01-19 16:35:45 +0000

    First commit

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2020 Brainbean Apps (https://brainbeanapps.com)
+# Copyright 2020-2022 CorporateHub (https://corporatehub.eu)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "CRM Document",
+    "version": "16.0.1.0.1",
+    "category": "CRM",
+    "installable": True,
+    "application": False,
+    "summary": "Documents attached to leads",
+    "depends": ["crm","partner_document"],
+    "data": [
+        "views/lead.xml", 
+        ],
+}
